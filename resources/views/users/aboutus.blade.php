8f5726a594981557bd2201f382880dc29a336515
kwaghtongo <soulmg7@gmail.com>
2019-07-16 15:03:51 +0100

    see more on about page

--- resources/views/users/aboutus.blade.php
+++ resources/views/users/aboutus.blade.php
@@ -3,59 +3,103 @@
 @section('content')
 	<!-- container -->
 	<div class="container">
-
+        <ol class="breadcrumb">
+			<li><a href="/">Home</a></li>
+			<li class="active">About Us</li>
+		</ol>
 		
 		<div class="row">
 			
 			<!-- Article main content -->
 			<article class="col-xs-12 maincontent">
 				<header class="page-header">
-		<ol class="breadcrumb">
-			<li><a href="index.html">About US</a></li>
-			<li class="active">About Us</li>
-		</ol>
+					<h1 class="page-title">About us</h1>
 				</header>
-				
-				<div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-					<div class="panel panel-default">
-						<div class="panel-body">
-							<h3 class="thin text-center">Sign in to your account</h3>
-							<p class="text-center text-muted">Lorem ipsum dolor sit amet, <a href="/signup">Register</a> adipisicing elit. Quo nulla quibusdam cum doloremque incidunt nemo sunt a tenetur omnis odio. </p>
-							<hr>
-							
-							<form method="POST" action="{{ route('login') }}">
-								@csrf
-								<div class="top-margin">
-									<label>Username/Email <span class="text-danger">*</span></label>
-									<input type="text" name="email" class="form-control">
-									@error('email')
-	                                    <span class="invalid-feedback" role="alert">
-	                                        <strong>{{ $message }}</strong>
-	                                    </span>
-	                                @enderror
-								</div>
-								<div class="top-margin">
-									<label>Password <span class="text-danger">*</span></label>
-									<input type="password" name="password" class="form-control">
-								</div>
-
-								<hr>
-
-								<div class="row">
-									<div class="col-lg-8">
-										<b><a href="">Forgot password?</a></b>
-									</div>
-									<div class="col-lg-4 text-right">
-										<button class="btn btn-action" type="submit">Sign in</button>
-									</div>
-								</div>
-							</form>
-						</div>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4">
+					      <img src="{{asset('images/cop1.png')}}">
+					     </div>
+					 	<div class="col-md-8">
+					 		<h2 style="color: blue;"> Welcome to MCCSL Cooperative Society </h2>
+					 		MCCSL is in true sense a credit cooperative society. It has been established in the year 2012 to meet the needs of the people based on agriculture and farming, it also initiates for the creation of jobs. On 20th September 2012, Moral Credit Cooperative Society is legally registered as multi-state cooperative society from the Agriculture Ministry of the Government of India and got its license to work in 10 states. It has its head office rooted in UP's capital Lucknow, the moral credit society is currently working in 10 states having more than 100 branches.
+					 
+					 	</div>	
 					</div>
-
-				</div>
-				
+					
+					 
+					 <h2 style="color: blue">Our Mission</h2>
+					 <p>
+					 	To strengthen the economic and social status of its members with financial co-operation and to provide a path to progressive and prosperous India by strengthening the cooperative movement. Providing opportunity to members to move forward for their economic growth and development.
+					 </p>
+					 <h3 style="color: blue">Our Vission</h3>
+					 <p>
+					 	For strengthening the economic status of the society members by keeping their interest on priority. MCCSL utilizes its work force, better service and uses new technology for its members through training, employment creation etc. for enhancing a sense of cooperativeness along its members and for building a prosperous and buoyant nation.
+					 </p>
+					 <h4 style="color: blue">Key Points</h4>
+					 <ul class="my-4">
+              	<li><span class="ion-ios-checkmark-circle mr-2"></span> Registered Membersdo contribution Every Month</li>
+              	<li><span class="ion-ios-checkmark-circle mr-2"></span> We Give Loan To Our Member With 0% interast rate</li>
+              	<li><span class="ion-ios-checkmark-circle mr-2"></span> We share Profit At The End of The Year</li>
+					</div>
+						</div>
+					</div>		
 			</article>
+		<center><h1 style="color: red">Our Cooperative Profile</h1></center>
+			<div class="row">
+				<div class="col-sm-3">
+					<center>
+						<img src="{{asset('images/cop1.png')}}" width="400px" height="400px">
+						<h2 style="color: blue">Director</h2>
+						<p>Dr. Pius Lukeman </p>
+						<a href="#"><img src="{{asset('images/facebook.png')}}" alt="facebook" width="20px"></a>
+						<a href="#"><img src="{{asset('images/twitter.jpg')}}" alt="twitter" width="20px" ></a>
+						<a href="#"><img src="{{asset('images/instagram.jpg')}}" alt="instagram" width="30px"></a>
+
+					</center>
+				
+				</div>
+
+				<div class="col-sm-3">
+					<center>
+						<img src="{{asset('images/cop1.png')}}" width="400px" height="400px">
+						<h2 style="color: blue">Director</h2>
+						<p>Dr. Pius Lukeman </p>
+						<a href="#"><img src="{{asset('images/facebook.png')}}" alt="facebook" width="20px"></a>
+						<a href="#"><img src="{{asset('images/twitter.jpg')}}" alt="twitter" width="20px" ></a>
+						<a href="#"><img src="{{asset('images/instagram.jpg')}}" alt="instagram" width="30px"></a>
+
+					</center>
+				
+				</div>
+
+				<div class="col-sm-3">
+					<center>
+						<img src="{{asset('images/cop1.png')}}" width="400px" height="400px">
+						<h2 style="color: blue">Director</h2>
+						<p>Dr. Pius Lukeman </p>
+						<a href="#"><img src="{{asset('images/facebook.png')}}" alt="facebook" width="20px"></a>
+						<a href="#"><img src="{{asset('images/twitter.jpg')}}" alt="twitter" width="20px" ></a>
+						<a href="#"><img src="{{asset('images/instagram.jpg')}}" alt="instagram" width="30px"></a>
+
+					</center>
+				
+				</div>
+				<div class="col-sm-3">
+					<center>
+						<img src="{{asset('images/cop1.png')}}" width="400px" height="400px">
+						<h2 style="color: blue">Director</h2>
+						<p>Dr. Pius Lukeman </p>
+						<a href="#"><img src="{{asset('images/facebook.png')}}" alt="facebook" width="20px"></a>
+						<a href="#"><img src="{{asset('images/twitter.jpg')}}" alt="twitter" width="20px"></a>
+						<a href="#"><img src="{{asset('images/instagram.jpg')}}" alt="instagram" width="30px"></a>
+
+					</center>
+				
+				</div>
+
+
+			</div>
 			<!-- /Article -->
 
 		</div>
